6ca71e5a99b6921fd1fef169ed57dda6084df8dd
Den Skvortsov <48939274+skvortsovden@users.noreply.github.com>
2021-12-02 16:37:19 +0100

    Add html generator

--- main.py
+++ main.py
@@ -35,6 +35,13 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
     def do_GET(self):
         self._set_headers()
         self.wfile.write(self._html("new-relic-logs-forwarder"))
