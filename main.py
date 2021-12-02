f4878038daec16baeb0efb9ec97970cf2384f1ba
Den Skvortsov <48939274+skvortsovden@users.noreply.github.com>
2021-12-02 16:33:56 +0100

    Add HEAD handler

--- main.py
+++ main.py
@@ -30,12 +30,20 @@ class HeaderlessLogAPI:
 
 class HttpHandler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+    def _set_headers(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._html("new-relic-logs-forwarder"))
+
+    def do_HEAD(self):
+        self._set_headers()
+
     def do_POST(self):
+        self._set_headers()
         if self.path == '/logs':
             content_len = int(self.headers.get('Content-Length'))
             post_body = self.rfile.read(content_len)
